f4957b771b7a6ed9c4bc7b84eaca682cffede81b
MatiasEncarnacion <matias.encarnacion@unl.edu.ec>
2025-12-14 22:29:41 -0500

    Ejercicios: Matías Encarnación

--- src/main/java/edu/unl/cc/succesion/business/PrimeNumberCalculatorUpToLimit.java
+++ src/main/java/edu/unl/cc/succesion/business/PrimeNumberCalculatorUpToLimit.java
@@ -1,7 +1,7 @@
 package edu.unl.cc.succession.business;
 
 import edu.unl.cc.succession.domain.Successionable;
-//autor Matias Encarnación
+//
 public class PrimeNumberCalculatorUpToLimit implements Successionable {
     private Integer limit;
     private Integer currentPrime;
@@ -67,4 +67,5 @@ public class PrimeNumberCalculatorUpToLimit implements Successionable {
         return printableTerms.toString();
     }
 }
+
 
